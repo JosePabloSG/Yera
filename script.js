f1d606ec81f121afbe15f56b56698bac41f4ff3a
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-03-21 20:24:23 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+onload = () => {
+  document.body.classList.remove("container");
+  alert("Te quiero mucho mi amor, eres lo mejor que me ha pasado en la vida, gracias por todo lo que haces por mi, por ser mi compañera, mi amiga");
+};
